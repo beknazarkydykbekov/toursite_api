4473b9ca141ef0680db61f3083a5611648fba199
beknazarkydykbekov <kydykbekovbeknazar@gmail.com>
2023-04-23 00:30:07 +0600

    local host 3000

--- toursite/settings.py
+++ toursite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'toursite.wsgi.application'
 
 # CORS WHITELIST
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8000",
+    "http://localhost:3000",
     "https://relaxed-curie-e9a516.netlify.app",
     "http://127.0.0.1:8080"
 ]
