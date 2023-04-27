4d4eb0ae9d72d36e170f5eb5ebda5dd4dceaae18
beknazarkydykbekov <kydykbekovbeknazar@gmail.com>
2023-04-27 19:33:30 +0600

    delete cors origin settings

--- toursite/settings.py
+++ toursite/settings.py
@@ -148,12 +148,6 @@ USE_TZ = True
 # CORS
 CORS_ALLOWED_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:9000",
-]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://change.allowed.com",
